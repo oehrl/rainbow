af38124f4a3f148e0567076fbb5254e87076cb6b
Simon Oehrl <oehrl@vr.rwth-aachen.de>
2019-06-03 12:13:21 +0200

    Add simple window via SDL2

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,16 @@
+#include <SDL2/SDL.h>
 #include <chrono>
 #include <iostream>
 #include "scene.hpp"
 
 int main(int argc, char* argv[]) {
+  SDL_Window* window;
+  SDL_Renderer* renderer;
+  if (SDL_CreateWindowAndRenderer(800, 600, 0, &window, &renderer) < 0) {
+    std::cerr << "Failed to create window!" << std::endl;
+    return -1;
+  }
+
   rainbow::Scene scene(argv[1]);
 
   const auto t0 = std::chrono::steady_clock::now();
@@ -13,4 +21,14 @@ int main(int argc, char* argv[]) {
   std::cout
       << std::chrono::duration_cast<std::chrono::milliseconds>(t1 - t0).count()
       << "ms" << std::endl;
+
+  SDL_Event event;
+  while (SDL_WaitEvent(&event)) {
+    if (event.type == SDL_QUIT) {
+      break;
+    }
+  }
+
+  SDL_DestroyRenderer(renderer);
+  SDL_DestroyWindow(window);
 }
