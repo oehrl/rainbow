04c66454ca85b1bfbb3c32c2d45f9eab435705f4
Simon Oehrl <oehrl@vr.rwth-aachen.de>
2019-06-10 12:09:08 +0200

    Remove context flags

--- src/application.cpp
+++ src/application.cpp
@@ -13,10 +13,6 @@ namespace rainbow {
 Application::Application() {
   SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 4);
   SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 3);
-  SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
-  SDL_GL_SetAttribute(
-      SDL_GL_CONTEXT_FLAGS,
-      SDL_GL_CONTEXT_FORWARD_COMPATIBLE_FLAG | SDL_GL_CONTEXT_DEBUG_FLAG);
 
   if (SDL_CreateWindowAndRenderer(512, 512, SDL_WINDOW_OPENGL, &window_,
                                   &renderer_) < 0) {
