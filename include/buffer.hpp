bb33a5179bca9e560da32b90b3e6cbf277e1f7ad
Simon Oehrl <oehrl@vr.rwth-aachen.de>
2019-06-10 12:08:21 +0200

    Add ShaderStorageBuffer

--- include/buffer.hpp
+++ include/buffer.hpp
@@ -12,9 +12,30 @@ class Buffer {
 
   Buffer& operator=(const Buffer&) = delete;
 
+  void Bind() { glBindBuffer(type_, gl_buffer_); }
+  void BindToIndex(GLuint index) { glBindBufferBase(type_, index, gl_buffer_); }
+
  private:
   GLuint gl_buffer_;
   GLenum type_;
 };
+
+struct ShaderStorageBufferDescription {
+  size_t size;
+};
+
+class ShaderStorageBuffer : public Buffer {
+ public:
+  ShaderStorageBuffer(const ShaderStorageBufferDescription& description,
+                      const void* data = nullptr)
+      : Buffer(GL_SHADER_STORAGE_BUFFER) {
+    Bind();
+    glBufferData(GL_SHADER_STORAGE_BUFFER,
+                 static_cast<GLsizeiptr>(description.size), data,
+                 GL_STATIC_DRAW);
+  }
+}
+
+;
 
 }  // namespace rainbow
