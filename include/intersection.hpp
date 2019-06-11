235e1aea7a35ab9b961503f22a9ba11988b5db9f
Simon Oehrl <oehrl@vr.rwth-aachen.de>
2019-06-11 12:57:13 +0200

    Simplified intersection code

--- include/intersection.hpp
+++ include/intersection.hpp
@@ -1,6 +1,8 @@
 #pragma once
 
+#include <array>
 #include <cassert>
+#include <glm/geometric.hpp>
 #include <glm/vec3.hpp>
 #include <glm/vector_relational.hpp>
 #include <optional>
@@ -22,6 +24,12 @@ inline glm::vec3 CalculateCenter(const Triangle& triangle) {
          (triangle.vertices[0] + triangle.vertices[1] + triangle.vertices[2]);
 }
 
+inline glm::vec3 CalculateNormal(const Triangle& triangle) {
+  return glm::normalize(
+      glm::cross(triangle.vertices[1] - triangle.vertices[0],
+                 triangle.vertices[2] - triangle.vertices[0]));
+}
+
 struct AxisAlignedBoundingBox {
   glm::vec3 min;
   glm::vec3 max;
@@ -32,9 +40,34 @@ inline glm::vec3 CalculateExtend(const AxisAlignedBoundingBox& aabb) {
   return aabb.max - aabb.min;
 }
 
+inline glm::vec3 CalculateHalfExtend(const AxisAlignedBoundingBox& aabb) {
+  assert(glm::all(glm::lessThanEqual(aabb.min, aabb.max)));
+  return CalculateExtend(aabb) * 0.5f;
+}
+
 inline glm::vec3 CalculateCenter(const AxisAlignedBoundingBox& aabb) {
   assert(glm::all(glm::lessThanEqual(aabb.min, aabb.max)));
   return (aabb.min + aabb.max) * 0.5f;
+}
+
+inline std::array<glm::vec3, 8> CalculateVertexPositions(
+    const AxisAlignedBoundingBox& aabb) {
+  assert(glm::all(glm::lessThanEqual(aabb.min, aabb.max)));
+  const auto center = CalculateCenter(aabb);
+  const auto half_extend = CalculateHalfExtend(aabb);
+
+  // clang-format off
+  return {
+    center + (glm::vec3{-1.0f, -1.0f, -1.0f} * half_extend),
+    center + (glm::vec3{-1.0f, -1.0f,  1.0f} * half_extend),
+    center + (glm::vec3{-1.0f,  1.0f, -1.0f} * half_extend),
+    center + (glm::vec3{-1.0f,  1.0f,  1.0f} * half_extend),
+    center + (glm::vec3{ 1.0f, -1.0f, -1.0f} * half_extend),
+    center + (glm::vec3{ 1.0f, -1.0f,  1.0f} * half_extend),
+    center + (glm::vec3{ 1.0f,  1.0f, -1.0f} * half_extend),
+    center + (glm::vec3{ 1.0f,  1.0f,  1.0f} * half_extend)
+  };
+  // clang-format on
 }
 
 struct RayTriangleIntersection {
--- src/intersection.cpp
+++ src/intersection.cpp
@@ -23,17 +23,6 @@ std::optional<RayTriangleIntersection> ComputeRayTriangleIntersection(
 
 bool CheckForTriangleAxisAlignedBoundingBoxIntersection(
     const Triangle& triangle, const AxisAlignedBoundingBox& aabb) {
-  glm::vec3 triangle_normal =
-      glm::normalize(glm::cross(triangle.vertices[1] - triangle.vertices[0],
-                                triangle.vertices[2] - triangle.vertices[0]));
-
-  float triangle_min, triangle_max;
-  float box_min, box_max;
-
-  // Test the box normals (x-, y- and z-axes)
-  const glm::vec3 box_normals[]{glm::vec3(1, 0, 0), glm::vec3(0, 1, 0),
-                                glm::vec3(0, 0, 1)};
-
   auto project = [](const auto& points, const glm::vec3& axis, float* min,
                     float* max) {
     assert(min != nullptr);
@@ -47,39 +36,52 @@ bool CheckForTriangleAxisAlignedBoundingBoxIntersection(
     }
   };
 
+  // Test the box normals (x-, y- and z-axes)
+  const glm::vec3 aabb_normals[]{glm::vec3(1, 0, 0), glm::vec3(0, 1, 0),
+                                 glm::vec3(0, 0, 1)};
   for (int i = 0; i < 3; i++) {
-    project(triangle.vertices, box_normals[i], &triangle_min, &triangle_max);
+    float triangle_min, triangle_max;
+    project(triangle.vertices, aabb_normals[i], &triangle_min, &triangle_max);
     if (triangle_max < aabb.min[i] || triangle_min > aabb.max[i]) {
-      return false;  // No intersection possible.
+      // No intersection possible.
+      return false;
     }
   }
 
   // Test the triangle normal
-  float triangle_offset = glm::dot(triangle_normal, triangle.vertices[0]);
-  std::vector<glm::vec3> box_vertices(8);
-  for (int i = 0; i < 8; ++i) {
-    glm::vec3 f((i / 4) % 2 == 0 ? 1 : -1, (i / 2) % 2 == 0 ? 1 : -1,
-                i % 2 == 0 ? 1 : -1);
-    box_vertices[i] = CalculateCenter(aabb) + 0.5f * f * CalculateExtend(aabb);
+  const glm::vec3 triangle_normal = CalculateNormal(triangle);
+  const float triangle_offset = glm::dot(triangle_normal, triangle.vertices[0]);
+  const auto aabb_vertices = CalculateVertexPositions(aabb);
+  {
+    float aabb_min, aabb_max;
+    project(aabb_vertices, triangle_normal, &aabb_min, &aabb_max);
+    if (aabb_max < triangle_offset || aabb_min > triangle_offset) {
+      // No intersection possible.
+      return false;
+    }
   }
-  glm::vec3 normal = glm::cross(triangle.vertices[1] - triangle.vertices[0],
-                                triangle.vertices[2] - triangle.vertices[0]);
-  project(box_vertices, glm::normalize(normal), &box_min, &box_max);
-  if (box_max < triangle_offset || box_min > triangle_offset)
-    return false;  // No intersection possible.
 
   // Test the nine edge cross-products
-  glm::vec3 triangle_edges[] = {triangle.vertices[0] - triangle.vertices[1],
-                                triangle.vertices[1] - triangle.vertices[2],
-                                triangle.vertices[2] - triangle.vertices[0]};
+  const glm::vec3 triangle_edges[] = {
+      triangle.vertices[0] - triangle.vertices[1],
+      triangle.vertices[1] - triangle.vertices[2],
+      triangle.vertices[2] - triangle.vertices[0],
+  };
   for (int i = 0; i < 3; i++) {
     for (int j = 0; j < 3; j++) {
       // The box normals are the same as it's edge tangents
-      glm::vec3 axis = glm::cross(triangle_edges[i], box_normals[j]);
-      project(box_vertices, axis, &box_min, &box_max);
+      const glm::vec3 axis = glm::cross(triangle_edges[i], aabb_normals[j]);
+
+      float aabb_min, aabb_max;
+      project(aabb_vertices, axis, &aabb_min, &aabb_max);
+
+      float triangle_min, triangle_max;
       project(triangle.vertices, axis, &triangle_min, &triangle_max);
-      if (box_max < triangle_min || box_min > triangle_max)
-        return false;  // No intersection possible
+
+      if (aabb_max < triangle_min || aabb_min > triangle_max) {
+        // No intersection possible
+        return false;
+      }
     }
   }
 
