87d9b2e7793034eac617af818082a2fcb555c551
Simon Oehrl <oehrl@vr.rwth-aachen.de>
2019-06-14 11:40:06 +0200

    Parallelize raytracing of CPU backend

--- src/backends/cpu/cpu_backend.cpp
+++ src/backends/cpu/cpu_backend.cpp
@@ -1,17 +1,19 @@
 #include "rainbow/backends/cpu/cpu_backend.hpp"
-#include <glm/vec3.hpp>
 #include "rainbow/camera.hpp"
 #include "rainbow/scene.hpp"
+#include "rainbow/thread_pool.hpp"
+#include "rainbow/timing.hpp"
 #include "rainbow/viewport.hpp"
+#include <glm/vec3.hpp>
 
 namespace rainbow {
 
-void CPUBackend::Prepare(const Scene& scene, size_t viewport_width,
+void CPUBackend::Prepare(const Scene &scene, size_t viewport_width,
                          size_t viewport_height) {
   scene_ = &scene;
 }
 
-void CPUBackend::Render(const Camera& camera, Viewport* viewport) {
+void CPUBackend::Render(const Camera &camera, Viewport *viewport) {
   glm::vec3 right;
   glm::vec3 up;
   glm::vec3 forward;
@@ -20,24 +22,28 @@ void CPUBackend::Render(const Camera& camera, Viewport* viewport) {
   const size_t viewport_width = viewport->GetWidth();
   const size_t viewport_height = viewport->GetHeight();
 
-  for (size_t y = 0; y < viewport_height; ++y) {
-    const float y_normalized =
-        static_cast<float>(y) / (viewport_height - 1) - 0.5f;
-    for (size_t x = 0; x < viewport_width; ++x) {
-      const float x_normalized =
-          static_cast<float>(x) / (viewport_width - 1) - 0.5f;
+  RAINBOW_TIME_SECTION("CPU Ray Tracing") {
+    // for (size_t y = 0; y < viewport_height; ++y) {
+    ParallelFor(static_cast<size_t>(0), viewport_height, [&](size_t y) {
+      const float y_normalized =
+          static_cast<float>(y) / (viewport_height - 1) - 0.5f;
+      for (size_t x = 0; x < viewport_width; ++x) {
+        const float x_normalized =
+            static_cast<float>(x) / (viewport_width - 1) - 0.5f;
 
-      const auto view_ray_direction =
-          glm::normalize(x_normalized * right + y_normalized * up + forward);
+        const auto view_ray_direction =
+            glm::normalize(x_normalized * right + y_normalized * up + forward);
 
-      const auto hitpoint =
-          scene_->ShootRay({camera.GetPosition(), view_ray_direction});
+        const auto hitpoint =
+            scene_->ShootRay({camera.GetPosition(), view_ray_direction});
 
-      if (hitpoint) {
-        viewport->SetPixel(x, y, hitpoint->material->diffuse);
+        if (hitpoint) {
+          viewport->SetPixel(x, y, hitpoint->material->diffuse);
+        }
       }
-    }
-  }
+    });
+    // }
+  };
 }
 
-}  // namespace rainbow
+} // namespace rainbow
